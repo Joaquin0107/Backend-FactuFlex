92474bd5f5592bd07f5a6caa7d6ca7bb5d2fa593
Joaquín Emilio Huayllasco López <joaquin20040107@gmail.com>
2024-11-21 12:36:03 -0500

    Cambios

--- src/main/java/pe/upc/edu/tffactuflexbackend/Repositories/ITREARepository.java
+++
@@ -1,7 +0,0 @@
-package pe.upc.edu.tffactuflexbackend.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.upc.edu.tffactuflexbackend.Entities.Tasas;
-
-public interface ITREARepository extends JpaRepository<Tasas.TREA, Long> {
-}
